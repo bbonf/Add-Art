edac47ffae145cb417aa121560bb7acfa3651047
slambert <slambert@b7a5d539-5f39-dc11-9877-00e0184e816e>
2008-06-01 02:31:23 +0000

    adding logging requests

--- addart/chrome/content/versionsplash.js
+++ addart/chrome/content/versionsplash.js
@@ -1,7 +1,12 @@
 //this file checks to see if the add-on has been updated and, if true, will load a page on add-art.org
 
 var addonart = {
+	
+	
   init: function AA_init() {
+	    var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
+	                                 .getService(Components.interfaces.nsIConsoleService);
+	  consoleService.logStringMessage("Checking if versionsplash has started");
     // The preference services gives us access to about:config stuff
     var prefs = Components.classes["@mozilla.org/preferences-service;1"]
                              .getService(Components.interfaces.nsIPrefService);
@@ -14,15 +19,20 @@ var addonart = {
     var addon = em.getItemForID("development@add-art.org");
     var currentVersion = addon.version;
     var lastVersion = null;
+	  consoleService.logStringMessage("current");
+	  consoleService.logStringMessage(currentVersion);
+
     if (prefs.prefHasUserValue("currentVersion"))
       lastVersion = prefs.getCharPref("currentVersion");
 
     if (!lastVersion) {/* This is the first time this was installed */
+	consoleService.logStringMessage("First time this was installed");
       prefs.setCharPref("currentVersion", currentVersion);
       var url = "http://add-art.org/version.php?ver=" + currentVersion;
       gBrowser.selectedTab = gBrowser.addTab(url, null);
     } else if (versionChecker.compare(currentVersion,lastVersion) > 0) {
       /* just an updated version... */
+      consoleService.logStringMessage("updatedVersion");
       prefs.setCharPref("currentVersion", currentVersion);
       var url = "http://add-art.org/version.php?ver=" + currentVersion + "&prev=" + lastVersion;
       gBrowser.selectedTab = gBrowser.addTab(url, null);
